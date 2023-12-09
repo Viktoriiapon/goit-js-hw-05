7584d690fc5af5f5b7f33da80389b47b6b6ecf93
Viktoriiapon <viktoriyaponom@gmail.com>
2023-12-09 10:02:25 +0100

    all tasks

--- js/task-2.js
+++ js/task-2.js
@@ -1,16 +1,38 @@
-function makeArray(firstArray, secondArray, maxLength){
-   const newArr = [].concat(firstArray, secondArray);
-   if (newArr.length <= maxLength){
-      return newArr;
-   }
-   else {
-      return newArr.slice(0,maxLength);
-   }
+const getUsersWithFriend = (users, friendName) => {
+  return users.filter(user => user.friends.includes(friendName));
+};
+const allUsers = [
+  {
+    name: 'Moore Hensley',
+    friends: ['Sharron Pace'],
+  },
+  {
+    name: 'Sharlene Bush',
+    friends: ['Briana Decker', 'Sharron Pace'],
+  },
+  {
+    name: 'Ross Vazquez',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+  },
+  {
+    name: 'Elma Head',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+  },
+  {
+    name: 'Carey Barr',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+  },
+  {
+    name: 'Blackburn Dotson',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+  },
+  {
+    name: 'Sheree Anthony',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+  },
+];
+console.log(getUsersWithFriend(allUsers, 'Briana Decker'));
 
-}
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); 
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); 
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); 
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); 
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); 
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
+console.log(getUsersWithFriend(allUsers, 'Goldie Gentry'));
+
+console.log(getUsersWithFriend(allUsers, 'Adrian Cross'));
